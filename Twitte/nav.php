0b31b24c1083f92fc139414978108dcaf2fca44e
Marcin Nowak <nowaxik@gmail.com>
2022-02-28 21:33:45 +0100

    Plik z menu

---
+++ Twitte/nav.php
@@ -0,0 +1,6 @@
+<nav class="nav flex-column">
+    <a class="nav-link active" aria-current="page" href="twitte.php">Strona Główna</a>
+    <a class="nav-link" href="profil.php">Profil</a>
+    <a class="nav-link" href="#">Link</a>
+    <a class="nav-link" href="logout.php">Logout</a>
+</nav>
