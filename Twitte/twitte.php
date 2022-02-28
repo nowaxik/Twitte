3aea4508aad511466cdc12b8600c16b9813b0301
Marcin Nowak <nowaxik@gmail.com>
2022-02-28 21:35:11 +0100

    Zmienio pełną sekscje menu na plik z informacjami na temat menu

--- Twitte/twitte.php
+++ Twitte/twitte.php
@@ -23,12 +23,7 @@ include 'header.php';
                 ?>
 
                 <section>
-                    <nav class="nav flex-column">
-                        <a class="nav-link active" aria-current="page" href="profil.php">Profil</a>
-                        <a class="nav-link" href="#">Link</a>
-                        <a class="nav-link" href="#">Link</a>
-                        <a class="nav-link" href="logout.php">Logout</a>
-                    </nav>
+                    <?php include 'nav.php'; ?>
                 </section>
             </div>
         </div>
