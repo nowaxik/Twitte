c9d60f5fa8efbd265f177f55b573d0545edd6837
Marcin Nowak <nowaxik@gmail.com>
2022-02-28 20:44:27 +0100

    Dopisano dalszą część połączenia z serwerem i bazą danych

--- Twitte/connect.php
+++ Twitte/connect.php
@@ -3,4 +3,8 @@ $servername = "localhost"; //Nazwa serwera
 $username = "root"; //Nazwa użytkownika
 $password = ""; //Hasło do konta użytkownika
 $dbname = "twitte"; //Nazwa bazy danych
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
 ?>
