69417c114028270a815ed55dbdb1f5a4a6af100e
Marcin Nowak <nowaxik@gmail.com>
2022-03-04 00:03:13 +0100

    Zmiana nazwy pierwszej pozycji

--- Twitte/nav.php
+++ Twitte/nav.php
@@ -21,7 +21,7 @@
 <br>
 <br>
 <nav class="nav flex-column">
-    <a class="nav-link active" aria-current="page" href="twitte.php">Strona Główna</a>
+    <a class="nav-link active" aria-current="page" href="twitte.php">Home</a>
     <a class="nav-link" href="profil.php">Profil</a>
     <a class="nav-link" href="#">Link</a>
     <a class="nav-link" href="logout.php">Logout</a>
