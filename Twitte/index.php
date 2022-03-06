de7d5648b1237bd47a7fc7cae54fca3c4daa4584
Marcin Nowak <nowaxik@gmail.com>
2022-03-06 16:40:21 +0100

    Dodano logo na formularzem i zmieniono wygląd

--- Twitte/index.php
+++ Twitte/index.php
@@ -6,18 +6,22 @@ include 'header.php';
 <section class="container-sm">
     <div class="row">
         <div class="formAuto">
-            <h3>Panel logowania</h3>
-            <form action="login.php" method="post">
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Podaj login:</label><br> 
-                    <input class="form-control" id="exampleInputEmail1" type="text" name="login" />
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Podaj hasło:</label><br>
-                    <input class="form-control" id="exampleInputEmail1" type="password" name="pass" /><br>
-                    <button type="submit"name="send" class="btn btn-primary">Wyślij</button>
-                </div>
+            <div class="mx-auto" style="width: 100px;">
+                <img src="icon/twitter64.png" class="logoLogin">
+            </div>
+            <div class="form">
+                <form action="login.php" method="post">
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Podaj login:</label><br> 
+                        <input class="form-control" id="exampleInputEmail1" type="text" name="login" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Podaj hasło:</label><br>
+                        <input class="form-control" id="exampleInputEmail1" type="password" name="pass" /><br>
+                        <button type="submit"name="send" class="btn btn-primary">Wyślij</button>
+                    </div>
             </form>
+            </div>
         </div>
     </div>
 </section>
