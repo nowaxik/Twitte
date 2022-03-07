0bedf647179c457449a049da7e280ab9fd882780
Marcin Nowak <nowaxik@gmail.com>
2022-03-07 20:51:16 +0100

    Przeniesienie zbędnych skryptów

--- Działające skrypty/Tworzenie bazy i tabeli automatycznie/index.php
+++
@@ -1,9 +0,0 @@
-<!-- Formularz dodawania imienia i nazwiska -->
-<form action="addDB.php" method="post">
-    
-    <label for="fname">Nazwa bazy danych:</label><br>
-    <input type="text" id="fname" name="dbname"><br>
-    
-    <input type="submit" value="Dodaj" name="add">
-
-  </form>
