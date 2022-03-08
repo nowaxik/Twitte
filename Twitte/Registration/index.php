ca73651cb2c8b5c8b6f27e3a0e14eb9daec01efd
Marcin Nowak <nowaxik@gmail.com>
2022-03-09 00:05:30 +0100

    Zmiana wyglądu w formularzu rejestracyjnym oraz skopiowanie pliku style.css do katalogu Rejestracja

--- Twitte/Registration/index.php
+++ Twitte/Registration/index.php
@@ -3,22 +3,31 @@ include '../header.php';
 ?>
 
 
-<header>
-        <h3>Zarejestruj się:</h3>
-        <form action="reg.php" method="post">
-            <label for="fname">Wpisz login:</label><br>
-            <input type="text" name="username" /><br>
-            <label for="fname">Wpisz hasło:</label><br>
-            <input type="password" name="pass" /><br>
-            <label for="fname">Wpisz nazwę jaka się będzie wyświetlać:</label><br>
-            <input type="text" name="userview" /><br>
-            <label for="fname">Wpisz adres e-mail:</label><br>
-            <input type="email" name="email" /><br>
-            <label for="fname">Wpisz datę urodzenia (YYYY-MM-DD):</label><br>
-            <input type="date" name="dateBir" /><br>
-            <input type="submit" value="Wyślij" name="send" />
-        </form>
-</header>
+<section class="container-sm">
+    <div class="row">
+        <div class="formAuto">
+            <div class="mx-auto" style="width: 100px;">
+                <img src="../icon/twitter64.png" class="logoLogin">
+            </div>
+            <div class="form">
+                <h3>Zarejestruj się:</h3>
+                <form action="reg.php" method="post">
+                    <label for="exampleInputEmail1" class="form-label">Wpisz login:</label><br>
+                    <input class="form-control" id="exampleInputEmail1" type="text" name="username" /><br>
+                    <label for="exampleInputEmail1" class="form-label">Wpisz hasło:</label><br>
+                    <input class="form-control" id="exampleInputEmail1" type="password" name="pass" /><br>
+                    <label for="exampleInputEmail1" class="form-label">Wpisz nazwę jaka się będzie wyświetlać:</label><br>
+                    <input class="form-control" id="exampleInputEmail1" type="text" name="userview" /><br>
+                    <label for="exampleInputEmail1" class="form-label">Wpisz adres e-mail:</label><br>
+                    <input class="form-control" id="exampleInputEmail1" type="email" name="email" /><br>
+                    <label for="exampleInputEmail1" class="form-label">Wpisz datę urodzenia (YYYY-MM-DD):</label><br>
+                    <input class="form-control" id="exampleInputEmail1" type="date" name="dateBir" /><br>
+                    <button type="submit"name="send" class="btn btn-primary">Wyślij</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section
 
 <?php
 include '../footer.php';
