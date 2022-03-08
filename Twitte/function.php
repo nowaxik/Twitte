22952774413f9b867bf4b021baa9e50a2e3ae280
Marcin Nowak <nowaxik@gmail.com>
2022-03-08 23:42:24 +0100

    Dodanie komentarzy oraz usunięcie nieużywanych funkcji

--- Twitte/function.php
+++ Twitte/function.php
@@ -1,31 +1,7 @@
 <?php
 
-include 'connect.php';
-
-
-function PostImage() 
-{
-    
-    $user_id = $_SESSION['id'];
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    
-            $PostImg = "SELECT img FROM users WHERE id=$user_id";
-            $image = $conn->query($PostImg);
-    
-            if ($image->num_rows > 0) {
-            // output data of each row
-            while($pos = $image->fetch_assoc()) {
-      ?>
-                                
-        <img src="img/<?php echo $pos["img"]; ?>" class="imgRadius" alt="Zdjecie profilowe" width="24" height="24">
-    
-      <?php
-        }
-      }
-}
-
+//Funkcja wyświetla zarejestrowanych użykowników
+//oraz ich status online i offline
 function ShowUsers() {
   
   include 'connect.php';
@@ -54,7 +30,5 @@ function ShowUsers() {
    $conn->close();
 
 }
-
-
 
 ?>
