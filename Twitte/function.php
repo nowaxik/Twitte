8e781228ac2a17de792154fcc5b328b3c779809a
Marcin Nowak <nowaxik@gmail.com>
2022-03-08 22:18:46 +0100

    Dodanie funkcji do wyświetlania użytkowników

--- Twitte/function.php
+++ Twitte/function.php
@@ -8,7 +8,7 @@ function PostImage()
     $user_id = $_SESSION['id'];
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
-          }
+    }
     
             $PostImg = "SELECT img FROM users WHERE id=$user_id";
             $image = $conn->query($PostImg);
@@ -23,6 +23,31 @@ function PostImage()
       <?php
         }
       }
+}
+
+function ShowUsers() {
+  
+  include 'connect.php';
+
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  
+        $sql = "SELECT username FROM users";
+        $result = $conn->query($sql);
+  
+          if ($result->num_rows > 0) {
+          // output data of each row
+            while($row = $result->fetch_assoc()) {
+              echo "Zarejestrowani użytkownicy:<br>";
+              echo "@".$row["username"]."<br>";
+            }
+              } else {
+                echo "0 results";
+              }
+  
+   $conn->close();
+
 }
 
 
