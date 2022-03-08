3a01338af7a90e16c962e27bacad5dcda6c5242b
Marcin Nowak <nowaxik@gmail.com>
2022-03-08 23:31:33 +0100

    Dodanie do funkcji ShowUsers statusu online oraz offline

--- Twitte/function.php
+++ Twitte/function.php
@@ -2,6 +2,7 @@
 
 include 'connect.php';
 
+
 function PostImage() 
 {
     
@@ -28,19 +29,23 @@ function PostImage()
 function ShowUsers() {
   
   include 'connect.php';
-
+  
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-  
-        $sql = "SELECT username FROM users";
+  echo "Zarejestrowani użytkownicy:<br>";
+        $sql = "SELECT status, username FROM users";
         $result = $conn->query($sql);
-  
+
           if ($result->num_rows > 0) {
           // output data of each row
             while($row = $result->fetch_assoc()) {
-              echo "Zarejestrowani użytkownicy:<br>";
-              echo "@".$row["username"]."<br>";
+                if ($row['status'] == 'online') {
+                  echo "@".$row["username"]."  <img src='icon/twitter-online.png'><br>";
+                  }
+                  else {
+                    echo "@".$row["username"]."  <img src='icon/twitter-offline.png'><br>";
+                  }
             }
               } else {
                 echo "0 results";
