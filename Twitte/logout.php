b402c7fa7d0b6a82c1c19d61e8c981f406ef2fb6
Marcin Nowak <nowaxik@gmail.com>
2022-03-08 23:32:19 +0100

    Zmiana statusu na offline po wylogowaniu

--- Twitte/logout.php
+++ Twitte/logout.php
@@ -1,8 +1,22 @@
 <?php
 
 	session_start();
+	$updateID = $_SESSION['id'];
 	
-	session_unset();
+	include 'connect.php';
+
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	  }
+	  
+	$sql = "UPDATE users SET status='offline' WHERE id=$updateID";
+	if ($conn->query($sql) === TRUE) {
+		echo "Record updated successfully";
+	  } else {
+		echo "Error updating record: " . $conn->error;
+	  }
+
+	session_destroy();
 	
 	header('Location: index.php');
 
